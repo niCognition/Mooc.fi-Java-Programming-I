7e78b9a09d19fe3aca48f11c8b683294f5762abe
niCognition <Niclas.p1990@gmail.com>
2020-02-19 14:17:19 +0100

    Added more exercises.

---
+++ Part 1/Part01_07.MessageThreeTimes/MessageThreeTimes.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class MessageThreeTimes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String message = scanner.nextLine();
+
+        System.out.println("Write a message:");
+        // Write your program here
+
+        System.out.println(message);
+        System.out.println(message);
+        System.out.println(message);
+
+    }
+}
