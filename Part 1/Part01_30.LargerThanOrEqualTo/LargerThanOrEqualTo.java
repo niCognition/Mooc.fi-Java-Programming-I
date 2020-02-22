126ce2722cfc9184f4882292b59e9ecdbef7934d
niCognition <Niclas.p1990@gmail.com>
2020-02-22 11:06:30 +0100

    Exercises

---
+++ Part 1/Part01_30.LargerThanOrEqualTo/LargerThanOrEqualTo.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class LargerThanOrEqualTo {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Give the first number:");
+        int first = Integer.parseInt(scan.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.parseInt(scan.nextLine());
+        if (first < second) {
+            System.out.println("Greater number is: "+second);
+        } else if (first > second) {
+            System.out.println("Greater number is: "+first);
+        } else if (first == second) {
+            System.out.println("The numbers are equal!");
+        }
+
+    }
+}
