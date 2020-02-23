cd9239781fef02a36ce22d643ee0b5b93de8fa2e
niCognition <Niclas.p1990@gmail.com>
2020-02-23 17:05:47 +0100

    Moar exercises

---
+++ Part 2/Part02_02.SquareRootOfSum/SquareRootOfSum.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class SquareRootOfSum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int first = Integer.parseInt(scanner.nextLine());
+        int second = Integer.parseInt(scanner.nextLine());
+
+        double squareRoot = Math.sqrt(first+second);
+        System.out.println(squareRoot);
+    }
+}
