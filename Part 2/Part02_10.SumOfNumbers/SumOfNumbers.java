31f88248fa55e5333642f1737085f2e648cbf0e5
niCognition <Niclas.p1990@gmail.com>
2020-02-26 22:11:18 +0100

    .

---
+++ Part 2/Part02_10.SumOfNumbers/SumOfNumbers.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class SumOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int input = Integer.parseInt(scanner.nextLine());
+
+            if (input == 0) {
+                break;
+            }
+            sum = sum + input;
+        }
+        System.out.println("Sum of the numbers: " + sum);
+    }
+}
