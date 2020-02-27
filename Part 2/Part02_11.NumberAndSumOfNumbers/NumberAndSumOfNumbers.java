d021c1fd281ceecf6360ef0803f7684abc684c59
niCognition <Niclas.p1990@gmail.com>
2020-02-27 16:40:39 +0100

    tldr; Exercisesgit add .

---
+++ Part 2/Part02_11.NumberAndSumOfNumbers/NumberAndSumOfNumbers.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class NumberAndSumOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numbers = 0;
+        int sum = 0;
+
+        while (true) {
+            System.out.println("Give a number:");
+            int input = Integer.parseInt(scanner.nextLine());
+
+            if (input == 0) {
+                break;
+            }
+
+            numbers = numbers + 1;
+            sum = sum + input;
+
+        }
+
+        System.out.println("Number of numbers: " + numbers);
+        System.out.println("Sum of the numbers: " + sum);
+    }
+}
