d6b2641eeddb1aad26a9c4a02a965858fc49b678
niCognition <Niclas.p1990@gmail.com>
2020-02-28 15:03:17 +0100

    E-e-e-e-exercises!

---
+++ Part 2/Part02_15.CountingToHundred/CountingToHundred.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class CountingToHundred {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int input = Integer.parseInt(scanner.nextLine());
+
+        for (int i = input; i <= 100; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
