c0fd547fd9742258f70b41d58e70dd5fdd5b8dc4
niCognition <Niclas.p1990@gmail.com>
2020-06-20 13:48:42 +0200

    find string in arraylist
    Used a for loop to loop through the arraylist, I then realized that I
    should've just used .contains() instead. So I'll be using that in the
    future instead.

---
+++ Part 3/Part03_14.OnTheList/OnTheList.java
@@ -0,0 +1,37 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class OnTheList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            list.add(input);
+        }
+
+        System.out.println("Search for? ");
+        String input = scanner.nextLine();
+        String found = null;
+
+        for (String entries : list) {
+            if (entries.equals(input)) {
+                found = entries;
+            }
+        }
+
+        if (found != null) {
+            System.out.println(found + " was found!");
+        } else {
+            System.out.println(input + " was not found!");
+        }
+
+    }
+}
