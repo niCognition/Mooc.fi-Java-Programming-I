45f21c200a0d3c618c076e719978b286491d14ef
niCognition <Niclas.p1990@gmail.com>
2020-06-22 19:26:32 +0200

    use method to print sum of array

---
+++ Part 3/Part03_20.SumOfArray/SumOfArray.java
@@ -0,0 +1,21 @@
+
+public class SumOfArray {
+
+    public static void main(String[] args) {
+        // You can try the method here
+        int[] array = {5, 1, 3, 4, 2};
+        System.out.println(sumOfNumbersInArray(array));
+    }
+
+    public static int sumOfNumbersInArray(int[] array) {
+        // Write some code here
+        int index = 0;
+        int sum = 0;
+        while (index < array.length) {
+            int number = array[index];
+            sum = sum + number;
+            index = index + 1;
+        }
+        return sum;
+    }
+}
