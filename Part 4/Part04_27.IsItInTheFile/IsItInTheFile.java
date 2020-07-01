89d7cb8ebcc3236fb0c622c43132a89c52c20942
niCognition <Niclas.p1990@gmail.com>
2020-07-02 01:21:36 +0200

    check if file contains input string

---
+++ Part 4/Part04_27.IsItInTheFile/IsItInTheFile.java
@@ -0,0 +1,34 @@
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class IsItInTheFile {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Name of the file:");
+        String file = scanner.nextLine();
+
+        System.out.println("Search for:");
+        String searchedFor = scanner.nextLine();
+
+        ArrayList<String> list = new ArrayList<>();
+
+        try (Scanner fileReader = new Scanner(Paths.get(file))) {
+            while (fileReader.hasNextLine()) {
+                list.add(fileReader.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println("Reading the file " + file + " failed.");
+        }
+
+        if (list.contains(searchedFor)) {
+            System.out.println("Found!");
+        } else {
+            System.out.println("Not found.");
+        }
+    }
+}
