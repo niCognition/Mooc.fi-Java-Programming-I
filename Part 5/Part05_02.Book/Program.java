277d0c3c916443d0cec0ef741f3166936b320bab
niCognition <Niclas.p1990@gmail.com>
2020-07-03 00:19:35 +0200

    create book class

---
+++ Part 5/Part05_02.Book/Program.java
@@ -0,0 +1,12 @@
+
+public class Program {
+
+    public static void main(String[] args) {
+        // This is simply an empty main method where you are
+        // free to experiment with your Book class.
+        // An example that you can use for testing:
+
+        Book tcoc = new Book("H.P.Lovecraft", "The Call of Cthulhu", 368);
+        System.out.println(tcoc);
+    }
+}
