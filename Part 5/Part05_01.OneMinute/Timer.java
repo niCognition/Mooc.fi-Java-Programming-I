ef8cab8b15deea975dc4e8a4482e31e07b00a561
niCognition <Niclas.p1990@gmail.com>
2020-07-03 00:19:03 +0200

    create timer class

---
+++ Part 5/Part05_01.OneMinute/Timer.java
@@ -0,0 +1,21 @@
+public class Timer {
+    private ClockHand seconds;
+    private ClockHand hundredths;
+
+    public Timer() {
+        this.seconds = new ClockHand(60);
+        this.hundredths = new ClockHand(100);
+    }
+
+    public void advance() {
+        this.hundredths.advance();
+
+        if (this.hundredths.value() == 0) {
+            this.seconds.advance();
+        }
+    }
+
+    public String toString() {
+        return this.seconds + ":" + this.hundredths;
+    }
+}
