869f481b3103692c005e46ffe8ffc07612f964b8
niCognition <Niclas.p1990@gmail.com>
2020-07-09 19:47:20 +0200

    create equals method in person class

---
+++ Part 5/Part05_13.IdenticalTwins/Person.java
@@ -0,0 +1,43 @@
+
+import java.util.Objects;
+
+public class Person {
+
+    private String name;
+    private SimpleDate birthday;
+    private int height;
+    private int weight;
+
+    public Person(String name, SimpleDate birthday, int height, int weight) {
+        this.name = name;
+        this.birthday = birthday;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    // implement an equals method here for checking the equality of objects
+    public boolean equals(Object compared) {
+        if (this == compared) {
+            return true;
+        }
+
+        if (!(compared instanceof Person)) {
+            return false;
+        }
+
+        Person comparedPerson = (Person) compared;
+
+        if (this.name.equals(comparedPerson.name) &&
+                this.birthday.getYear() == comparedPerson.birthday.getYear() &&
+                this.birthday.getMonth() == comparedPerson.birthday.getMonth() &&
+                this.birthday.getDay() == comparedPerson.birthday.getDay() &&
+                this.height == comparedPerson.height &&
+                this.weight == comparedPerson.weight) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+}
